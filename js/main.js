62073c79a47efb451789569e83a2b99876408963
Lewis <38163383+iBeef@users.noreply.github.com>
2026-01-13 14:30:53 +0000

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import DeleteTableModal from "./classes/DeleteTableModal.js";
+import SchemaControl from "./classes/SchemaControl.js";
+
+const deleteTableModal = new DeleteTableModal();
+deleteTableModal.run();
+
+const schemaControl = new SchemaControl();
+schemaControl.run();
